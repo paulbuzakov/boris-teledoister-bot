04d140765290acd17585aaaffd31f37a59848841
Paul Buzakov <paul@smartpuppy.local>
2024-03-10 17:07:07 +0500

    init: Initialize Teledoist

---
+++ src/Teledoist.Api/Controllers/TokenController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Teledoist.Api.Controllers;
+
+[ApiController]
+[Route("api/teledoist/[controller]")]
+public class TokenController : ControllerBase
+{
+}
---
+++ src/Teledoist.Api/Controllers/WebHookController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Teledoist.Api.Controllers;
+
+[ApiController]
+[Route("api/teledoist/[controller]")]
+public class WebHookController : ControllerBase
+{
+    private const string _HELLO_MESSAGE = "Hello World!!!";
+
+    public WebHookController() { }
+
+    [HttpPost("payload")]
+    public string Payload()
+    {
+        return _HELLO_MESSAGE;
+    }
+}
